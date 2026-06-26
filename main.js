b01cbf6b7a7c2472fe934d85e47a0a5815a3da86
stephaniesilva <stephanienathaliasilvabaron08@gmail.com>
2026-06-25 20:35:56 -0500

    feat: agregando

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ document.addEventListener("product-submit", (event) => {
   items.push(item);
   localStorage.setItem("items", JSON.stringify(items));
 });
+
+const user = {
+  username: "nata", 
+  password: "123abc"
+  }
+
+  const URL_BASE = "https://stock-flow-3accf-default-rtdb.firebaseio.com"
+
+  const httpClient = fetch(`${URL_BASE}/user.json`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+  
+    body: JSON.stringify(user)
+  });
+  
+  const res = httpclient.then(data => data.json());
+  res.then(data => console.log).catch(err => console.error)
